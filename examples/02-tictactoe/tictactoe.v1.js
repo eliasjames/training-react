97d3a1514dc0d05d4b32311e8206d9c081bab327
Elias Carlston <elias@eliascarlston.com>
2018-01-15 17:17:29 -0500

    First Tictactoe example

---
+++ examples/02-tictactoe/tictactoe.v1.js
@@ -0,0 +1,56 @@
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare() {
+    return <Square />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare()}
+          {this.renderSquare()}
+          {this.renderSquare()}
+        </div>
+        <div className="board-row">
+          {this.renderSquare()}
+          {this.renderSquare()}
+          {this.renderSquare()}
+        </div>
+        <div className="board-row">
+          {this.renderSquare()}
+          {this.renderSquare()}
+          {this.renderSquare()}
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+function renderWhenReady(){
+	ReactDOM.render(
+		<Board/>,
+		document.getElementById('root')
+	);
+}
+
+if( document.readyState === 'complete' ) {
+	renderWhenReady();
+} else {
+	document.addEventListener( 'DOMContentLoaded', ()=>{
+		renderWhenReady();
+	});
+}
