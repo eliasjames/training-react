3d9f14e197b6755e7b32dd948acc39e3849e6a4a
Elias Carlston <elias@eliascarlston.com>
2018-01-15 21:56:09 -0500

    Make hardcoded rows dynamic

---
+++ examples/02-tictactoe/tictactoe.v1.1.js
@@ -0,0 +1,51 @@
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare() {
+    return <Square />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+    let boardLayout = [], 
+			eachRowLayout = [];
+
+    for ( let i=0; i<3; i++ ) {
+      eachRowLayout.push( renderSquare() );
+    }
+    for ( let i=0; i<3; i++ ) {
+      boardLayout.push( <div key={i} className="board-row">{ eachRowLayout }</div> );
+    }
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+				{ boardLayout }
+      </div>
+    );
+  }
+}
+
+// ========================================
+function renderWhenReady(){
+	ReactDOM.render(
+		<Board/>,
+		document.getElementById('root')
+	);
+}
+
+if( document.readyState === 'complete' ) {
+	renderWhenReady();
+} else {
+	document.addEventListener( 'DOMContentLoaded', ()=>{
+		renderWhenReady();
+	});
+}
