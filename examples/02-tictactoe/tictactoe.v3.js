bd74abb53978586523334e88b50083e3b9c88328
Elias Carlston <elias@eliascarlston.com>
2018-01-16 10:52:55 -0500

    Add click handler to JSX element

---
+++ examples/02-tictactoe/tictactoe.v3.js
@@ -0,0 +1,54 @@
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square" onClick={()=>{ console.log( 'clicked ', this.props.value ); }}>
+        { this.props.value }
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare( i ) {
+    return <Square value={i} key={i} />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+    let boardLayout = [], 
+			eachRowLayout = [],
+			squareCounter = 0;
+
+    for ( let i=0; i<3; i++ ) {
+			for ( let i=0; i<3; i++ ) {
+				eachRowLayout.push( this.renderSquare( squareCounter ));
+				squareCounter++;
+			}
+      boardLayout.push( <div key={i} className="board-row">{ eachRowLayout }</div> );
+			eachRowLayout = [];
+    }
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+				{ boardLayout }
+      </div>
+    );
+  }
+}
+
+// ========================================
+function renderWhenReady(){
+	ReactDOM.render(
+		<Board/>,
+		document.getElementById('root')
+	);
+}
+
+if( document.readyState === 'complete' ) {
+	renderWhenReady();
+} else {
+	document.addEventListener( 'DOMContentLoaded', ()=>{
+		renderWhenReady();
+	});
+}
