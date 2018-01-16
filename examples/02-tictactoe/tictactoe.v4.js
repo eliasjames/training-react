94b96341dd246acddf8d52eded9030cde9da9b3a
Elias Carlston <elias@eliascarlston.com>
2018-01-16 14:10:40 -0500

    Add state to Square

---
+++ examples/02-tictactoe/tictactoe.v4.js
@@ -0,0 +1,61 @@
+class Square extends React.Component {
+  constructor( props ) {
+    super( props );
+    this.state = {
+      value: null,
+    };
+  }
+
+  render() {
+    return (
+      <button className="square" onClick={ ()=>{ this.setState({value: 'X'}); }}>
+        { this.state.value }
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare( i ) {
+    return <Square value={i} key={i} />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+    let boardLayout = [], 
+			eachRowLayout = [],
+			squareCounter = 0;
+
+    for ( let i=0; i<3; i++ ) {
+			for ( let i=0; i<3; i++ ) {
+				eachRowLayout.push( this.renderSquare( squareCounter ));
+				squareCounter++;
+			}
+      boardLayout.push( <div key={i} className="board-row">{ eachRowLayout }</div> );
+			eachRowLayout = [];
+    }
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+				{ boardLayout }
+      </div>
+    );
+  }
+}
+
+// ========================================
+function renderWhenReady(){
+	ReactDOM.render(
+		<Board/>,
+		document.getElementById('root')
+	);
+}
+
+if( document.readyState === 'complete' || document.readyState === 'interactive' ) {
+	renderWhenReady();
+} else {
+	document.addEventListener( 'DOMContentLoaded', ()=>{
+		renderWhenReady();
+	});
+}
